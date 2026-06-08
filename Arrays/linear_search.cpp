fdf03718419f6df044538a564050314a4af18fea
Priyanshu Choubey <choubeypriyanshu2110@gmail.com>
2026-06-08 20:59:01 +0530

    Implement linear search function in C++
    Fix linear search implementation and add input prompts.

--- Arrays/linear_search.cpp
+++ Arrays/linear_search.cpp
@@ -1,1 +1,32 @@
+#include<iostream>
+using namespace std;
 
+int linear_search(int arr[] , int n , int key){
+    for(int i=0 ; i<n ; i++){
+        if(arr[i] == key){
+            cout<<key<<" found at index ";
+            return i;
+        }
+    }
+    return -1;
+}
+
+int main(){
+
+    int size;
+    cout<<"Enter size: "<<endl;
+    cin>>size;
+
+    int arr[size] ; 
+
+    for(int i=0 ; i<size ; i++){
+        cin>>arr[i];
+    }
+
+    int key;
+    cout<<"enter value of key: "<<endl;
+    cin>>key;
+    
+    cout<<linear_search(arr , size , key);
+    return 0;
+}
